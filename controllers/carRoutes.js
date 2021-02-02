37e0541d9f3fd42306bb63d4fcba3ea7a1e17458
Gary Barkauski <gbarka2@gmail.com>
2021-02-02 15:30:07 -0700

    Add delete route

--- controllers/carRoutes.js
+++ controllers/carRoutes.js
@@ -19,16 +19,8 @@ router.put('/:id', async (req, res) => {
   res.json(await Car.findByIdAndUpdate(req.params.id, req.body, {new: true}))
 })
 
-// router.get('/cars/:id', async (req, res) => {
-//   res.json(await Car.find({_id: req.params.id}))
-// })
-
-// router.post('/cars', async (req, res) => {
-//   res.json(await Car.create(req.body))
-// })
-
-// router.put('/cars/:id', async (req, res) => {
-//   res.json(await Car.findByIdAndUpdate(req.params.id, req.body, {new: true}))
-// })
+router.delete('/:id', async (req, res) => {
+  res.json(await Car.findByIdAndDelete(req.params.id))
+})
 
 module.exports = router
