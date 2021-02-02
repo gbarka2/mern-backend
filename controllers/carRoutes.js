6d4457e729d21c8251322be7a1688054be1624c4
Gary Barkauski <gbarka2@gmail.com>
2021-02-02 15:02:49 -0700

    Add get by Id route

--- controllers/carRoutes.js
+++ controllers/carRoutes.js
@@ -2,14 +2,21 @@ const mongoose = require('../db/connection')
 const { Router } = require('express')
 const router = Router()
 const Car = require('../models/cars')
-const db = mongoose.connection
 
 router.get('/', async (req, res) => {
   res.json(await Car.find({}))
 })
 
+router.get('/cars/:id', async (req, res) => {
+  res.json(await Car.find({_id: req.params.id})
+})
+
 router.post('/cars', async (req, res) => {
   res.json(await Car.create(req.body))
+})
+
+router.put('/cars/:id', async (req, res) => {
+  res.json(await Car.findByIdAndUpdate(req.params.id, req.body, {new: true}))
 })
 
 module.exports = router
