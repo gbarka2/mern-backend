95e22a32b23e1d1c493ed0df6ed8a4e6aaae3b66
Gary Barkauski <gbarka2@gmail.com>
2021-02-02 15:04:42 -0700

    Correct get by id route

--- controllers/carRoutes.js
+++ controllers/carRoutes.js
@@ -8,8 +8,8 @@ router.get('/', async (req, res) => {
 })
 
 router.get('/cars/:id', async (req, res) => {
-  res.json(await Car.find({_id: req.params.id})
-})
+  res.json(await Car.find({_id: req.params.id}))
+}
 
 router.post('/cars', async (req, res) => {
   res.json(await Car.create(req.body))
