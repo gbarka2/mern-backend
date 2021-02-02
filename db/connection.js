86cfbe4d0ee3efc6eef784e04ccea2f754841efa
Gary Barkauski <gbarka2@gmail.com>
2021-02-02 13:13:40 -0700

    Correct MONGODBURI

--- db/connection.js
+++ db/connection.js
@@ -1,8 +1,9 @@
 require("dotenv").config()
 const mongoose = require('mongoose')
-const mongoURI = 'mongodb://localhost:27017/cars'
+const {MONGODBURI} = process.env
+// const mongoURI = 'mongodb://localhost:27017/cars'
 const config = {useUnifiedTopology: true, useNewUrlParser: true}
-mongoose.connect(mongoURI, config)
+mongoose.connect(MONGODBURI, config)
 
 const db = mongoose.connection
 db.on('error', err => console.log(err.message + 'is mongod running'))
