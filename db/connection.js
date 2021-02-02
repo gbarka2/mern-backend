4e945da3e5c3ed5d1c92ffe0916e8a3e9faa8002
Gary Barkauski <gbarka2@gmail.com>
2021-02-02 13:27:05 -0700

    MongoDBURI update

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ mongoose.connect(MONGODBURI, config)
 
 const db = mongoose.connection
 db.on('error', err => console.log(err.message + 'is mongod running'))
-db.on('connected', () => console.log('mongo connected: ', mongoURI))
+db.on('connected', () => console.log('mongo connected: ', MONGODBURI))
 db.on('disconnected', () => console.log('mongo disconnected'))
 
 module.exports = mongoose
