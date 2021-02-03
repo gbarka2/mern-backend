867c60b5c9b30fe7380a85708dc9e8c787a417f1
Gary Barkauski <gbarka2@gmail.com>
2021-02-03 08:23:07 -0700

    Update put route

--- controllers/ownerRoutes.js
+++ controllers/ownerRoutes.js
@@ -1,11 +1,12 @@
 const mongoose = require('../db/connection')
 const Owner = require('../models/owners')
+const Car = require('../models/cars')
 const { Router } = require('express')
 const router = Router()
 const db = mongoose.connection
 
 router.get('/', async (req, res) => {
-  res.json(await Owner.find({}))
+  res.json(await Owner.find({}).populate('cars'))
 })
 
 router.get('/:id', async (req, res) => {
@@ -20,9 +21,14 @@ router.put('/:id', async (req, res) => {
   res.json(await Owner.findByIdAndUpdate(req.params.id, req.body, {new: true}))
 })
 
+router.put('/:id/:carId', async (req, res) => {
+  const car = await Car.findById(req.params.carId)
+  const owner = await Owner.findByIdAndUpdate(req.params.id, {$push: {cars: car.id}, new: true})
+  // const owner = res.json(await Owner.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+})
+
 router.delete('/:id', async (req, res) => {
   res.json(await Owner.findByIdAndDelete(req.params.id))
 })
-
 
 module.exports = router
