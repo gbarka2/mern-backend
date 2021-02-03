46a8d58a5824b1f4c853d2737f24a11bda02cb22
Gary Barkauski <gbarka2@gmail.com>
2021-02-02 19:26:12 -0700

    Add update, get by id, and delete route

--- controllers/ownerRoutes.js
+++ controllers/ownerRoutes.js
@@ -8,8 +8,20 @@ router.get('/', async (req, res) => {
   res.json(await Owner.find({}))
 })
 
+router.get('/:id', async (req, res) => {
+  res.json(await Owner.find({_id: req.params.id}))
+})
+
 router.post('/', async (req, res) => {
   res.json(await Owner.create(req.body))
+})
+
+router.put('/:id', async (req, res) => {
+  res.json(await Owner.findByIdAndUpdate(req.params.id, req.body, {new: true}))
+})
+
+router.delete('/:id', async (req, res) => {
+  res.json(await Owner.findByIdAndDelete(req.params.id))
 })
 
 
